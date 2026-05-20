6f8cad8f12933e5654070c94f706ea4ecbfecff1
preetiwork33-hard <preetiwork33@gmail.com>
2026-05-20 21:55:08 +0530

    added frontend  .end render url/api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
-const api = axios.create({ baseURL: API_URL });
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+
+});
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('klaahub_token');
